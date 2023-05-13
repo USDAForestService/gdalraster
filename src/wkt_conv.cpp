8f881245c85dedc4c715d14d3293fa172b3cadcc
Chris Toney <jctoney@gmail.com>
2023-05-12 20:27:28 -0600

    wkt_conv.cpp: add srs_to_wkt()

--- src/wkt_conv.cpp
+++ src/wkt_conv.cpp
@@ -34,6 +34,9 @@
 //' for display to a person. `FALSE` for a regular WKT string (the default).
 //' @return Character string containing OGC WKT.
 //'
+//' @seealso
+//' [srs_to_wkt()]
+//'
 //' @examples
 //' epsg_to_wkt(5070)
 //' writeLines(epsg_to_wkt(5070, pretty=TRUE))
@@ -48,6 +51,85 @@ std::string epsg_to_wkt(int epsg, bool pretty = false) {
 	
 	if (OSRImportFromEPSG(hSRS, epsg) != OGRERR_NONE)
 		Rcpp::stop("Error importing from EPSG code.");
+	
+	if (pretty) {
+		if (OSRExportToPrettyWkt(hSRS, &pszSRS_WKT, false) != OGRERR_NONE) {
+			OSRDestroySpatialReference(hSRS);
+			Rcpp::stop("Error exporting to pretty WKT.");
+		}
+	}
+	else {
+		if (OSRExportToWkt(hSRS, &pszSRS_WKT) != OGRERR_NONE) {
+			OSRDestroySpatialReference(hSRS);
+			Rcpp::stop("Error exporting to WKT.");
+		}
+	}
+	
+	std::string wkt(pszSRS_WKT);
+	OSRDestroySpatialReference(hSRS);
+	CPLFree(pszSRS_WKT);
+	
+	return wkt;
+}
+
+//' Convert spatial reference definition to Well Known Text (WKT)
+//'
+//' `srs_to_wkt()` converts a spatial reference system (SRS) definition 
+//' in various text formats to WKT. The function will examine the input SRS, 
+//' try to deduce the format, and then export it to WKT.
+//'
+//' @details
+//' The input SRS may take the following forms:
+//'   * `WKT` - to convert WKT versions (see below)
+//'   * `EPSG:n` - EPSG code n
+//'   * \code{AUTO:proj_id,unit_id,lon0,lat0} - WMS auto projections
+//'   * `urn:ogc:def:crs:EPSG::n` - OGC urns
+//'   * PROJ.4 definitions
+//'   * `filename` - file read for WKT, XML or PROJ.4 definition
+//'   * well known name such as `NAD27`, `NAD83`, `WGS84` or `WGS72`
+//'   * `IGNF:xxxx`, `ESRI:xxxx` - definitions from the PROJ database
+//'   * PROJJSON (PROJ >= 6.2)
+//'
+//' This function is intended to be flexible, but by its nature it is 
+//' imprecise as it must guess information about the format intended. 
+//' [epsg_to_wkt()] should be used if the input is known to be an integer 
+//' EPSG code.
+//'
+//' As of GDAL 3.0, the default format for WKT export is OGC WKT 1.
+//' The WKT version can be overridden by using the `OSR_WKT_FORMAT` 
+//' configuration option (see [set_config_option()]).
+//' Valid values are one of: `SFSQL`, `WKT1_SIMPLE`, `WKT1`, `WKT1_GDAL`, 
+//' `WKT1_ESRI`, `WKT2_2015`, `WKT2_2018`, `WKT2`, `DEFAULT`.
+//' If `SFSQL`, a WKT1 string without AXIS, TOWGS84, AUTHORITY or 
+//' EXTENSION node is returned. If `WKT1_SIMPLE`, a WKT1 string without 
+//' AXIS, AUTHORITY or EXTENSION node is returned. `WKT1` is an alias of 
+//' `WKT1_GDAL`. `WKT2` will default to the latest revision implemented 
+//' (currently `WKT2_2018`). `WKT2_2019` can be used as an alias of 
+//' `WKT2_2018` since GDAL 3.2
+//'
+//' @param srs Character string containing an SRS definition in various
+//' formats (see Details).
+//' @param pretty Logical. `TRUE` to return a nicely formatted WKT string 
+//' for display to a person. `FALSE` for a regular WKT string (the default).
+//' @return Character string containing OGC WKT.
+//'
+//' @seealso
+//' [epsg_to_wkt()]
+//'
+//' @examples
+//' srs_to_wkt("NAD83")
+//' writeLines(srs_to_wkt("NAD83", pretty=TRUE))
+//' set_config_option("OSR_WKT_FORMAT", "WKT2")
+//' writeLines(srs_to_wkt("NAD83", pretty=TRUE))
+//' set_config_option("OSR_WKT_FORMAT", "")
+// [[Rcpp::export]]
+std::string srs_to_wkt(std::string srs, bool pretty = false) {
+
+	OGRSpatialReferenceH hSRS = OSRNewSpatialReference(NULL);
+	char *pszSRS_WKT = NULL;
+	
+	if (OSRSetFromUserInput(hSRS, srs.c_str()) != OGRERR_NONE)
+		Rcpp::stop("Error importing SRS from user input.");
 	
 	if (pretty) {
 		if (OSRExportToPrettyWkt(hSRS, &pszSRS_WKT, false) != OGRERR_NONE) {
