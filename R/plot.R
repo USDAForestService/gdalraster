c95f61b04274259a0c2995e6c52af938d85a762d
Chris Toney <jctoney@gmail.com>
2023-06-20 23:44:06 -0600

    plot.R: initial version

---
+++ R/plot.R
@@ -0,0 +1,110 @@
+# plot raster data that has been read into a vector
+
+#' @noRd
+.normalize <- function(x) {
+	return((x - min(x,na.rm=TRUE)) / (max(x,na.rm=TRUE) - min(x,na.rm=TRUE)))
+}
+
+#' @noRd
+.as_raster <- function(a, normalize=TRUE, col_scale_fn=NULL, 
+				col_na=rgb(0,0,0,0), ...) {
+
+	if ( !(dim(a)[3] %in% c(1,3)) )
+		stop("Number of bands must be 1 or 3")
+	
+    if (normalize)
+    	a <- .normalize(a)
+    	
+    if (is.null(col_scale_fn))
+        col_scale_fn <- ifelse (dim(a)[3] == 1, gray, rgb)
+    
+	nas <- is.na(a)
+	has.na <- any(nas)
+	if (has.na) {
+		a[nas] <- 0
+		if (dim(nas)[3] == 3)
+			nas <- nas[,,1] || nas[,,2] || nas[,,3]
+		dim(nas) <- dim(nas)[1:2]
+		nas <- t(nas)
+	}
+    
+	if (dim(a)[3] == 1) {
+		# grayscale
+		dim(a) <- dim(a)[1:2]
+		r <- col_scale_fn(a)
+		dim(r) <- dim(a)[2:1]
+		class(r) <- "raster"
+    }
+    else {
+    	# rgb
+		r <- col_scale_fn(a[,,1], a[,,2], a[,,3])
+		dim(r) <- dim(a)[2:1]
+		class(r) <- "raster"
+	}
+	
+	if (has.na)
+        r[nas] <- col_na
+	
+	return(r)
+}
+
+#' Display raster data using base graphics
+#'
+#' @param data
+#' @param xsize
+#' @param ysize
+#' @param nbands
+#' @param normalize Rescale pixel values so that their range is [0,1].
+#' @param col_scale_fn An optional color scale function (default is gray or rgb)
+#' @param xlim x plot limits (default: 1 to width)
+#' @param ylim y plot limits (default: 1 to height)
+#' @param xlab x axis label
+#' @param ylab y axis label
+#' @param interpolate Should the image be plotted with antialiasing (default TRUE).
+#' @param asp Aspect ratio.
+#' @param xaxs The style of axis interval calculation to be used for the x-axis. See ?par
+#' @param yaxs The style of axis interval calculation to be used for the y-axis. See ?par
+#' @param axes Whether to draw axes (default TRUE)
+#' @param col_na Color to use for `NA` as a 7 or 9-character hexadecimal code. The default is transparent (`"#00000000"`, the return value of `rgb(0,0,0,0)`).
+#' @param main Main title.
+#' @param ... Other parameters to be passed to `plot.default()`.
+#' @export
+plot_raster_data <- function(data, xsize, ysize, nbands=1,
+						normalize=TRUE, col_scale_fn=NULL,
+						xlim=c(1, xsize), ylim=c(ysize, 1),
+                 		xlab="x", ylab="y",
+                 		interpolate=TRUE, asp=1, axes=TRUE, main="",
+                 		xaxs="i", yaxs="i", col_na=rgb(0,0,0,0), ...) {
+
+	if ( !(nbands %in% c(1,3)) )
+		stop("Number of bands must be 1 or 3")
+		
+	if (dev.capabilities()$rasterImage == "no") {
+		message("Device does not support rasterImage.")
+		invisible(FALSE)
+	}
+	
+	if (length(data) == 0)
+		stop("length(data) == 0.")
+		
+	if (xsize < 1 || ysize < 1)
+		stop("Invalid x/y dimensions.")
+		
+	a <- array(data, dim = c(xsize, ysize, nbands))
+	r <- .as_raster(a,
+					normalize=normalize,
+					col_scale_fn=col_scale_fn,
+					col_na=col_na)
+
+	plot.new()
+	plot.window(xlim=xlim, ylim=ylim, asp=asp, xaxs=xaxs, yaxs=yaxs,...)
+	rasterImage(r, 1, nrow(r), ncol(r), 1, interpolate=interpolate)
+	title(main)
+	if (axes) {
+		axis(1)
+		axis(2)
+	}
+
+	invisible(TRUE)
+}
+
