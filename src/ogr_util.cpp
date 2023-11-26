e6cf36883062eba549c9044e444f0c8e8de74036
Chris Toney <jctoney@gmail.com>
2023-11-26 00:21:58 -0700

    add _create_ogr()

--- src/ogr_util.cpp
+++ src/ogr_util.cpp
@@ -6,9 +6,10 @@
 #include "gdal.h"
 #include "cpl_error.h"
 #include "cpl_port.h"
+#include "cpl_string.h"
 #include "ogr_srs_api.h"
 
-//' Does vector data source exist
+//' Does vector dataset exist
 //' 
 //' @noRd
 // [[Rcpp::export(name = ".ogr_ds_exists")]]
@@ -28,6 +29,99 @@ bool _ogr_ds_exists(std::string dsn, bool with_update = false) {
 
 	GDALClose(hDS);
 	return true;
+}
+
+//' Create a vector dataset with layer and field
+//' currently hard coded as layer of wkbPolygon, field of OFTInteger
+//'
+//' @noRd
+// [[Rcpp::export(name = ".create_ogr")]]
+bool _create_ogr(std::string format, std::string dst_filename,
+		int xsize, int ysize, int nbands, std::string dataType,
+		std::string layer, std::string srs = "", std::string fld_name = "",
+		Rcpp::Nullable<Rcpp::CharacterVector> dsco = R_NilValue,
+		Rcpp::Nullable<Rcpp::CharacterVector> lco = R_NilValue) {
+
+	GDALDriverH hDriver = GDALGetDriverByName( format.c_str() );
+	if (hDriver == NULL)
+		Rcpp::stop("Failed to get driver for the specified format.");
+		
+	char **papszMetadata = GDALGetMetadata(hDriver, NULL);
+	if (!CPLFetchBool(papszMetadata, GDAL_DCAP_CREATE, FALSE))
+		Rcpp::stop("Driver does not support create.");
+	
+	GDALDataType dt = GDALGetDataTypeByName( dataType.c_str() );
+	
+	std::vector<char *> opt_list = {NULL};
+	if (dsco.isNotNull()) {
+		Rcpp::CharacterVector dsco_in(dsco);
+		opt_list.resize(dsco_in.size() + 1);
+		for (R_xlen_t i = 0; i < dsco_in.size(); ++i) {
+			opt_list[i] = (char *) (dsco_in[i]);
+		}
+		opt_list[dsco_in.size()] = NULL;
+	}
+	
+	GDALDatasetH hDstDS = NULL;
+	hDstDS = GDALCreate(hDriver, dst_filename.c_str(),
+						xsize, ysize, nbands, dt,
+                    	opt_list.data());
+
+	if (hDstDS == NULL)
+		Rcpp::stop("Create dataset failed.");
+
+	if (!GDALDatasetTestCapability(hDstDS, ODsCCreateLayer)) {
+		GDALClose(hDstDS);
+		return false;
+	}
+
+	OGRLayerH  hLayer;
+	OGRFieldDefnH hFieldDefn;
+	bool layer_ok = false;
+	bool fld_ok = false;
+	
+	opt_list.clear();
+	if (lco.isNotNull()) {
+		Rcpp::CharacterVector lco_in(lco);
+		opt_list.resize(lco_in.size() + 1);
+		for (R_xlen_t i = 0; i < lco_in.size(); ++i) {
+			opt_list[i] = (char *) (lco_in[i]);
+		}
+	}
+	opt_list.push_back(NULL);
+	
+	OGRSpatialReferenceH hSRS = OSRNewSpatialReference(NULL);
+	if (srs != "") {
+		if (OSRSetFromUserInput(hSRS, srs.c_str()) != OGRERR_NONE) {
+			GDALClose(hDstDS);
+			Rcpp::stop("Error importing SRS from user input.");
+		}
+	}
+
+	hLayer = GDALDatasetCreateLayer(hDstDS, layer.c_str(), hSRS, wkbPolygon,
+				opt_list.data());
+	
+	if (hLayer == NULL) {
+		layer_ok = false;
+	}
+	else {
+		layer_ok = true;
+		hFieldDefn = OGR_Fld_Create(fld_name.c_str(), OFTInteger);
+		if (OGR_L_CreateField(hLayer, hFieldDefn, TRUE) != OGRERR_NONE)
+			fld_ok = false;
+		else
+			fld_ok = true;
+
+		OGR_Fld_Destroy(hFieldDefn);
+	}
+	
+	OSRDestroySpatialReference(hSRS);
+	GDALClose(hDstDS);
+	
+	if (layer_ok && fld_ok)
+		return true;
+	else
+		return false;
 }
 
 //' Get number of layers in a dataset
@@ -74,7 +168,7 @@ bool _ogr_layer_exists(std::string dsn, std::string layer) {
 	return ret;
 }
 
-//' Create a layer in a vector data source
+//' Create a layer in a vector dataset
 //' currently hard coded as layer of wkbPolygon
 //'
 //' @noRd
@@ -127,7 +221,7 @@ bool _ogr_layer_create(std::string dsn, std::string layer,
 	return ret;
 }
 
-//' Delete a layer in a vector data source
+//' Delete a layer in a vector dataset
 //'
 //' @noRd
 // [[Rcpp::export(name = ".ogr_layer_delete")]]
