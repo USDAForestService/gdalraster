795fc05c9fbfd27fb8dee9efe1885ce59f462977
Chris Toney <jctoney@gmail.com>
2024-02-20 20:58:19 -0700

    fix test for footprint() if GDAL < 3.8

--- tests/testthat/test-gdal_exp.R
+++ tests/testthat/test-gdal_exp.R
@@ -279,6 +279,8 @@ test_that("translate runs without error", {
 })
 
 test_that("footprint runs without error", {
+	skip_if(.gdal_version_num() < 3080000)
+	
 	evt_file <- system.file("extdata/storml_evt.tif", package="gdalraster")
 	args <- c("-t_srs", "EPSG:4326")
 	out_file <- paste0(tempdir(), "/", "storml.geojson")
