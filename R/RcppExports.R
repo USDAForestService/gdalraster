68ab182bbc990cf9d3d89f9b99f5504824c1911e
Chris Toney <jctoney@gmail.com>
2024-05-24 22:53:49 -0600

    Doc: update get_usable_physical_ram()

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -202,9 +202,16 @@ get_num_cpus <- function() {
 #' Get usable physical RAM
 #'
 #' `get_usable_physical_ram()` returns the total physical RAM, usable by a
-#' process, in bytes.
+#' process, in bytes. It will limit to 2 GB for 32 bit processes. Starting
+#' with GDAL 2.4.0, it will also take into account resource limits (virtual
+#' memory) on Posix systems. Starting with GDAL 3.6.1, it will also take into
+#' account RLIMIT_RSS on Linux.
 #'
-#' @return Numeric scalar, number of bytes as `bit64::integer64` type.
+#' @return Numeric scalar, number of bytes as `bit64::integer64` type (or 0 in
+#' case of failure).
+#'
+#' @note
+#' This memory may already be partly used by other processes.
 #'
 #' @examples
 #' get_usable_physical_ram()
