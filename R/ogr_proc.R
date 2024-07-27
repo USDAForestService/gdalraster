b3dd91619da27b5e06136094a4bf205c73c62c25
Chris Toney <jctoney@gmail.com>
2024-07-26 20:11:16 -0600

    add example for intersection

--- R/ogr_proc.R
+++ R/ogr_proc.R
@@ -5,10 +5,10 @@
 #'
 #' @description
 #' `ogr_proc()` performs various geoprocessing operations on vector layers
-#' (intersection, union, clip, etc). It provides an inteface to the `OGRLayer`
-#' methods for these operations in the GDAL API (`OGRLayer::Intersection()`,
-#' etc). Inputs are given as objects of class `GDALVector` which might have
-#' spatial and/or attribute filters applied.
+#' (intersection, union, clip, etc). It provides an inteface to the GDAL API
+#' methods for these operations (`OGRLayer::Intersection()`, etc).
+#' Inputs are given as objects of class [`GDALVector`][GDALVector] which
+#' might have spatial and/or attribute filters applied.
 #' The output layer will be created, but output can optionally be appended to an
 #' existing layer, or written to an empty layer that has a custom schema
 #' defined. The `ogr_proc()` interface is basically equivalent to the
@@ -49,12 +49,12 @@
 #'
 #' The schema of the output layer can be set by the user or, if it is empty, is
 #' initialized to contain all fields in the input layer. The functions in the
-#' [ogr_manage] interface can be used to create an empty layer with user-defined
-#' schema (e.g., [ogr_ds_create()], [ogr_layer_create()], [ogr_field_create()]).
-#' If the schema of the output layer is set by the user and contains fields that
-#' have the same name as a field in both the input and method layers, then the
-#' attribute for an output feature will get the value from the feature of the
-#' method layer.
+#' [`ogr_manage`][ogr_manage] interface can be used to create an empty layer
+#' with user-defined schema (e.g., [ogr_ds_create()], [ogr_layer_create()] and
+#' [ogr_field_create()]). If the schema of the output layer is set by the user
+#' and contains fields that have the same name as a field in both the input and
+#' method layers, then the attribute for an output feature will get the value
+#' from the feature of the method layer.
 #'
 #' Options that affect processing can be set as NAME=VALUE pairs passed in the
 #' `mode_opt` argument. Some options are specific to certain processing modes
@@ -81,18 +81,19 @@
 #' only if the result output has an UNKNOWN geometry type. Applies to
 #' `Intersection`, `Union`, `Identity`.
 #'
-#' The input layers should have the same spatial reference system. No on-the-fly
+#' The input layers should use the same spatial reference system. No on-the-fly
 #' reprojection is done. When a default output layer is created by `ogr_proc()`,
 #' it will have the SRS of `input_lyr`.
 #'
 #' @param mode Character string specifying the operation to perform. One of
 #' `Intersection`, `Union`, `SymDifference`, `Identity`, `Update`, `Clip` or
 #' `Erase` (see Details).
-#' @param input_lyr An object of class `GDALVector` to use as the input layer.
-#' For overlay operations, this is the first layer in the operation.
-#' @param method_lyr An object of class `GDALVector` to use as the method
-#' layer. This is the conditional layer supplied to an operation (e.g., Clip,
-#' Erase, Update), or the second layer in overlay operations (e.g., Union,
+#' @param input_lyr An object of class [`GDALVector`][GDALVector] to use as the
+#' input layer. For overlay operations, this is the first layer in the
+#' operation.
+#' @param method_lyr An object of class [`GDALVector`][GDALVector] to use as the
+#' method layer. This is the conditional layer supplied to an operation (e.g.,
+#' Clip, Erase, Update), or the second layer in overlay operations (e.g., Union,
 #' Intersection, SymDifference).
 #' @param out_dsn The destination vector filename or database connection string
 #' to which the output layer will be written.
@@ -117,17 +118,18 @@
 #' @param quiet Logical scalar. If `TRUE`, a progress bar will not be
 #' displayed. Defaults to `FALSE`.
 #' @param return_lyr_obj Logical scalar. If `TRUE` (the default), an object of
-#' class `GDALVector` opened on the ouput layer will be returned, otherwise
-#' returns a logical value.
+#' class [`GDALVector`][GDALVector] opened on the ouput layer will be returned,
+#' otherwise returns a logical value.
 #'
-#' @returns Upon successful completion, an object of class `GDALVector` is
-#' returned by default (if `return_lyr_obj = TRUE`), or logical `TRUE` is
-#' returned (invisibly) if `return_lyr_obj = FALSE`.
+#' @returns Upon successful completion, an object of class
+#' [`GDALVector`][GDALVector] is returned by default (if
+#' `return_lyr_obj = TRUE`), or logical `TRUE` is returned (invisibly) if
+#' `return_lyr_obj = FALSE`.
 #' Logical `FALSE` is returned (invisibly) if an error occurs during processing.
 #'
 #' @note
 #' For best performance use the minimum amount of features in the method layer
-#' and copy it into a memory layer.
+#' and copy into a memory layer.
 #'
 #' The first geometry field is always used.
 #'
@@ -136,10 +138,42 @@
 #' [QGIS](https://www.qgis.org/).
 #'
 #' @seealso
-#' [GDALVector], [ogr_define], [ogr_manage]
+#' [`GDALVector-class`][GDALVector], [`ogr_define`][ogr_define],
+#' [`ogr_manage`][ogr_manage]
 #'
 #' @examples
+#' # MTBS fires in Yellowstone National Park 1984-2022
+#' dsn <- system.file("extdata/ynp_fires_1984_2022.gpkg", package="gdalraster")
+#' lyr <- new(GDALVector, dsn, "mtbs_perims")
 #'
+#' # filter to the 1988 North Fork fire
+#' lyr$setAttributeFilter("incid_name = 'NORTH FORK'")
+#' lyr$getFeatureCount()
+#'
+#' # get a second layer of fires during 2000-2020
+#' sql <- "SELECT * FROM mtbs_perims WHERE ig_year >= 2000"
+#' lyr2 <- new(GDALVector, dsn, sql)
+#' lyr2$getFeatureCount()
+#'
+#' # intersect
+#' lyr_out <- ogr_proc(mode = "Intersection",
+#'                     input_lyr = lyr,
+#'                     method_lyr = lyr2,
+#'                     out_dsn = tempfile(fileext = ".gpkg"),
+#'                     out_lyr_name = "north_fork_reburned",
+#'                     out_geom_type = "MULTIPOLYGON",
+#'                     mode_opt = "PROMOTE_TO_MULTI=YES")
+#'
+#' # the result layer has attributes of both the input and the method layers
+#' lyr_out$returnGeomAs <- "TYPE_NAME"
+#' d <- lyr_out$fetch(-1)
+#' print(d)
+#'
+#' # clean up
+#' lyr$close()
+#' lyr2$close()
+#' lyr_out$close()
+#' lyr_out$getDsn() |> unlink()
 #' @export
 ogr_proc <- function(mode,
                      input_lyr,
