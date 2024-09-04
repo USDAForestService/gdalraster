25e7c06ab7ba987cc2a965a568d623e4332b1947
Chris Toney <jctoney@gmail.com>
2024-09-04 17:05:41 -0600

    skip test of ogr_layer_rename() if GDAL < 3.5

--- tests/testthat/test-ogr_manage.R
+++ tests/testthat/test-ogr_manage.R
@@ -83,11 +83,13 @@ test_that("OGR management utilities work", {
                    "geom"))
 
     # rename layer
-    expect_true(ogr_layer_test_cap(dsn, "test_layer3")$Rename)
-    expect_true(ogr_layer_rename(dsn, "test_layer3", "test_new_name"))
-    expect_equal(ogr_layer_field_names(dsn, "test_new_name"),
-                 c("field1", "field2", "field3", "field4", "field5", "field6",
-                   "geom"))
+    if (as.integer(gdal_version()[2]) >= 3050000) {
+        expect_true(ogr_layer_test_cap(dsn, "test_layer3")$Rename)
+        expect_true(ogr_layer_rename(dsn, "test_layer3", "test_new_name"))
+        expect_equal(ogr_layer_field_names(dsn, "test_new_name"),
+                     c("field1", "field2", "field3", "field4", "field5",
+                       "field6", "geom"))
+    }
 
     deleteDataset(dsn)
 
