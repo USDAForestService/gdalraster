928c1bcd60bea7b0479b74e3fd78abfccaaa624f
Chris Toney <jctoney@gmail.com>
2025-12-06 16:52:49 -0700

    vsi_read_dir(): fix slow string processing of the file list for return

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1844,10 +1844,10 @@ vsi_curl_clear_cache <- function(partial = FALSE, file_prefix = "", quiet = TRUE
 #' files whose name does not start with a dot). If `TRUE`, all file names
 #' will be returned.
 #' @returns A character vector containing the names of files and directories
-#' in the directory given by `path`. The listing is in alphabetical order, and
-#' does not include the special entries '.' and '..' even if they are present
-#' in the directory. An empty string (`""`) is returned if `path` does not
-#' exist.
+#' in the directory given by `path` (may be an empty vector `character(0)`).
+#' The listing is in alphabetical order, and does not include the special
+#' entries '.' and '..' even if they are present in the directory. An empty
+#' string (`""`) is returned if `path` does not exist.
 #'
 #' @note
 #' If `max_files` is set to a positive number, directory listing will stop
